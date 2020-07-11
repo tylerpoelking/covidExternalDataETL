a51b5d8f66b16efda2c29d7e0714dec7740fa6f5
Corbin Albert <corbin.albert@gmail.com>
2020-07-11 12:44:10 -0700

    Add setup.py. No dependencies as using conda for env mgmt

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='covid_impact',
+    packages=find_packages(),
+    version='0.1.0',
+    description='CPG Covid Impact Analysis and Projections for Mondelez',
+    author='IBM GBS Distribution',
+    author_email='corbin.albert@ibm.com',
+    url='github pages or github link, or whatever you are using for your project',
+    license='',
+)
